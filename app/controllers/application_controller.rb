9ea8759325a91daa1cd29ff46d8e3c1ed69e3908
Charles Swaleh <charlswall@live.com>
2023-04-03 15:23:01 +0300

    Created the POST request for the /reviews route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,4 +32,15 @@ class ApplicationController < Sinatra::Base
     # send a response with the deleted review as JSON
     review.to_json
   end
+
+  post "/reviews" do
+    review =
+      Review.create(
+        score: params[:score],
+        comment: params[:comment],
+        game_id: params[:game_id],
+        user_id: params[:user_id]
+      )
+    review.to_json
+  end
 end
